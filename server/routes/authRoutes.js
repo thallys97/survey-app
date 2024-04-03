f1297faae9c7294221c8ea67578b326c53e7714e
thallys97 <thallysbandeira97@gmail.com>
2024-04-03 16:05:30 -0500

    estabeleci a sessão do usuário na função de callback e criei rotas de /current_user e /logout

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -9,11 +9,27 @@ router.get('/google',
 
 // Callback da rota após autenticação
 router.get('/google/callback',
-  passport.authenticate('google', { failureRedirect: '/' }),
+  passport.authenticate('google', { failureRedirect: '/login' }),
   (req, res) => {
+    // Aqui você pode configurar o que será armazenado na sessão do usuário
+    req.session.user = req.user;
     res.redirect('/dashboard'); // Ou para onde você deseja direcionar o usuário
   }
 );
+
+router.get('/current_user', (req, res) => {
+  if (req.isAuthenticated()) {
+    res.send(req.user);
+  } else {
+    res.status(401).send({ user: null });
+  }
+});
+
+router.get('/logout', (req, res) => {
+  req.logout();  // Passport expõe este método para terminar uma sessão de login
+  req.session.destroy();  // Destrói a sessão no servidor
+  res.redirect('/');
+});
 
 // Exportar as rotas
 module.exports = router;
