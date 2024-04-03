c3058c75289cefb425331c48c1788e62862723aa
thallys97 <thallysbandeira97@gmail.com>
2024-04-03 11:57:56 -0500

    adicionei rotas para abrir, fechar um survey e outra rota para enviar resposta para um survey

--- server/routes/surveyRoutes.js
+++ server/routes/surveyRoutes.js
@@ -10,6 +10,13 @@ router.post('/', ensureAuth, ensureCoordinator, surveyController.createSurvey);
 // GET para listar todas as surveys
 router.get('/', surveyController.getAllSurveys);
 
-// Aqui você vai adicionar mais rotas conforme necessário
+// Endpoint para abrir uma survey
+router.put('/open/:id', ensureAuth, ensureCoordinator, surveyController.openSurvey);
+
+// Endpoint para fechar uma survey
+router.put('/close/:id', ensureAuth, ensureCoordinator, surveyController.closeSurvey);
+
+// Endpoint para submeter resposta para uma survey
+router.post('/response', ensureAuth, surveyController.submitResponse);
 
 module.exports = router;
