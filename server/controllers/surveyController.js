a9bb3a87b4eafded667b739ee06d42abec189992
thallys97 <thallysbandeira97@gmail.com>
2024-04-03 11:56:28 -0500

    adicionei lógicas para abrir, fechar e submeter resposta para um survey

--- server/controllers/surveyController.js
+++ server/controllers/surveyController.js
@@ -21,3 +21,39 @@ exports.getAllSurveys = async (req, res) => {
     res.status(500).json({ message: 'Error fetching surveys', error: error.message });
   }
 };
+
+// Abrir uma survey
+exports.openSurvey = async (req, res) => {
+  try {
+    const survey = await Survey.findByIdAndUpdate(req.params.id, { open: true }, { new: true });
+    res.status(200).json(survey);
+  } catch (error) {
+    res.status(500).json({ message: 'Error opening survey', error: error.message });
+  }
+};
+
+// Fechar uma survey
+exports.closeSurvey = async (req, res) => {
+  try {
+    const survey = await Survey.findByIdAndUpdate(req.params.id, { open: false }, { new: true });
+    res.status(200).json(survey);
+  } catch (error) {
+    res.status(500).json({ message: 'Error closing survey', error: error.message });
+  }
+};
+
+// Submeter resposta para uma survey
+exports.submitResponse = async (req, res) => {
+  try {
+    const { surveyId, responses } = req.body;
+    const newResponse = new Response({
+      survey: surveyId,
+      responses: responses,
+      respondent: req.user._id
+    });
+    await newResponse.save();
+    res.status(201).json(newResponse);
+  } catch (error) {
+    res.status(500).json({ message: 'Error submitting response', error: error.message });
+  }
+};
