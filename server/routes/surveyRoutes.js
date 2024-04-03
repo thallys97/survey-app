4075bf368b9c67a8255bf18b3eb79eb870c5b1ec
thallys97 <thallysbandeira97@gmail.com>
2024-04-03 11:19:42 -0500

    adicionei o autenticador ensureCoordinator ao surveyRoutes.js

--- server/routes/surveyRoutes.js
+++ server/routes/surveyRoutes.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const surveyController = require('../controllers/surveyController');
-const { ensureAuth } = require('../middlewares/authMiddleware');
+const { ensureAuth, ensureCoordinator } = require('../middlewares/authMiddleware');
+
 
 // POST para criar uma nova survey - apenas usuários autenticados
-router.post('/', ensureAuth, surveyController.createSurvey);
+router.post('/', ensureAuth, ensureCoordinator, surveyController.createSurvey);
 
 // GET para listar todas as surveys
 router.get('/', surveyController.getAllSurveys);
