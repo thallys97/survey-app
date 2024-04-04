6ac6d3407d75e85a0067ac12b6ee4e27ab6937ff
thallys97 <thallysbandeira97@gmail.com>
2024-04-04 18:20:49 -0500

    inclui o botão de logout no Dashboard

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import LogoutButton from './LogoutButton';
 
 const Dashboard = () => {
   return (
     <div>
       <h1>Dashboard</h1>
+      <LogoutButton /> {/* Inclua o botão de logout */}
       {/* Restante do seu componente */}
     </div>
   );
