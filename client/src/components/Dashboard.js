868f2e8a97ae9b9264852ac1f8f0b22790573ae2
thallys97 <thallysbandeira97@gmail.com>
2024-04-04 16:23:04 -0500

    criei um componente de Dashboard

---
+++ client/src/components/Dashboard.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Dashboard = () => {
+  return (
+    <div>
+      <h1>Dashboard</h1>
+      {/* Restante do seu componente */}
+    </div>
+  );
+};
+
+export default Dashboard;
