ccf4f2f0b63f0ae26c94fdd0ec0a525795c226ae
thallys97 <thallysbandeira97@gmail.com>
2024-04-04 14:56:13 -0500

    alterei o App.js para poder realizar o login na aplicação

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+import React, { useEffect } from 'react';
+import authService from './services/authService';
+import Login from './components/Login';
+// Importe outros componentes e serviços conforme necessário
 
-function App() {
+const App = () => {
+  useEffect(() => {
+    const fetchCurrentUser = async () => {
+      const user = await authService.fetchCurrentUser();
+      if (user) {
+        console.log('Usuário autenticado:', user);
+        // Redirecione ou atualize o estado do aplicativo conforme necessário
+      }
+    };
+
+    fetchCurrentUser();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Login />
+      {/* Outros componentes do seu aplicativo */}
     </div>
   );
-}
+};
 
 export default App;
