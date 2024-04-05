4c210f1c920d7e32cc3f77ca245bb0bffefe37c6
thallys97 <thallysbandeira97@gmail.com>
2024-04-05 14:26:20 -0500

    envolvi minhas rotas com o AuthProvider

--- client/src/App.js
+++ client/src/App.js
@@ -1,33 +1,22 @@
 //import logo from './logo.svg';
 import './App.css';
-import React, { useEffect } from 'react';
+import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import authService from './services/authService';
+import { AuthProvider } from './contexts/AuthContext';
 import Home from './components/Home';
 import Dashboard from './components/Dashboard';
-// Importe outros componentes e serviços conforme necessário
 
 const App = () => {
-  useEffect(() => {
-    const fetchCurrentUser = async () => {
-      const user = await authService.fetchCurrentUser();
-      if (user) {
-        console.log('Usuário autenticado:', user);
-        // Redirecione ou atualize o estado do aplicativo conforme necessário
-      }
-    };
-
-    fetchCurrentUser();
-  }, []);
-
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        {/* Outras rotas... */}
-      </Routes>
-    </Router>
+    <AuthProvider>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          {/* ...other routes */}
+        </Routes>
+      </Router>
+    </AuthProvider>
   );
 };
 
