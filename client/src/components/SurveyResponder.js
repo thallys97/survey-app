d7782d4597f3b8e0368571d81adf5713849486bb
thallys97 <thallysbandeira97@gmail.com>
2024-04-06 15:18:47 -0500

    Permitindo que o usuário responda a uma survey

--- client/src/components/SurveyResponder.js
+++ client/src/components/SurveyResponder.js
@@ -5,6 +5,32 @@ import axiosInstance from '../api/axiosInstance';
 const SurveyResponder = () => {
   const [surveysList, setSurveysList] = useState([]);
   const [loading, setLoading] = useState(true);
+  // State para a survey selecionada e suas perguntas
+const [selectedSurvey, setSelectedSurvey] = useState(null);
+const [answers, setAnswers] = useState({});
+
+const selectSurvey = async (surveyId) => {
+  try {
+    const response = await axiosInstance.get(`/api/surveys/${surveyId}`);
+    setSelectedSurvey(response.data);
+    setAnswers(
+      response.data.questions.reduce((acc, question) => {
+        acc[question._id] = '';
+        return acc;
+      }, {})
+    );
+  } catch (error) {
+    console.error('Erro ao selecionar survey', error);
+  }
+};
+
+const handleAnswerChange = (questionId, choice) => {
+  setAnswers({ ...answers, [questionId]: choice });
+};
+
+const isSurveyComplete = () => {
+  return Object.values(answers).every((answer) => answer !== '');
+};
 
   useEffect(() => {
     const fetchSurveys = async () => {
