74defe9787ce7f25c8fefec4a61788520d9f2596
thallys97 <thallysbandeira97@gmail.com>
2024-04-06 22:05:28 -0500

    adicionei o botão que redireciona para a rota SurveyResponder

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -11,11 +11,17 @@ const Dashboard = () => {
     navigate('/create-survey');
   };
 
+   
+   const handleRespondToSurvey = () => {
+    navigate('/respond-survey'); 
+  };
+
   return (
     <div>
       <h1>Dashboard</h1>
       {user && <p>Welcome, {user.displayName}!</p>}
-      <button onClick={handleCreateSurvey}>Criar Survey</button>
+      <button onClick={handleCreateSurvey}>Criar survey</button>
+      <button onClick={handleRespondToSurvey}>Checar surveys abertas</button>
       <LogoutButton />
     </div>
   );
