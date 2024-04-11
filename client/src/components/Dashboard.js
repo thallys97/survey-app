1c7737fd08b873b417cd9cc748da3b43f5bb96d1
thallys97 <thallysbandeira97@gmail.com>
2024-04-11 12:25:40 -0500

    estilizei o componente de dashboard

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -7,37 +7,30 @@ const Dashboard = () => {
   const { user } = useContext(AuthContext);
   const navigate = useNavigate();
 
-  const handleCreateSurvey = () => {
-    navigate('/create-survey');
-  };
-
-   
-   const handleRespondToSurvey = () => {
-    navigate('/respond-survey'); 
-  };
-
-  const handleWaitingSurveys = () => {
-    navigate('/waiting-surveys');
-  };
-
-  const handleCheckSurveyResults = () => {
-    navigate('/responded-surveys');
+  const handleNavigation = (path) => {
+    navigate(path);
   };
 
   return (
-    <div>
-      <h1>Dashboard</h1>
-      {user && <p>Welcome, {user.displayName}!</p>}
-      <button onClick={handleCreateSurvey}>Criar survey</button>
-      <button onClick={handleWaitingSurveys}>Surveys em Espera</button>
-      <button onClick={handleRespondToSurvey}>Checar surveys abertas</button>
-      <button onClick={handleCheckSurveyResults}>Checar resultados de surveys</button>
-      {user.role === 'SurveyAdmin' && (
-        <button onClick={() => navigate('/user-management')} className="p-2 bg-blue-500 text-white rounded mt-4">
-          Gerenciar Usuários
-        </button>
-      )}
-      <LogoutButton />
+    <div className="min-h-screen bg-gray-100 py-10">
+      <div className="container mx-auto px-4">
+        <div className="bg-white p-6 rounded-lg shadow-lg">
+          <h1 className="text-2xl font-bold mb-4 text-gray-800">Dashboard</h1>
+          {user && <p className="mb-6">Bem-vindo, {user.displayName}!</p>}
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-4 mb-4">
+            <button onClick={() => handleNavigation('/create-survey')} className="bg-blue-500 text-white py-2 px-4 rounded-md hover:bg-blue-600 transition duration-300 font-bold">Criar Survey</button>
+            <button onClick={() => handleNavigation('/waiting-surveys')} className="bg-green-500 text-white py-2 px-4 rounded-md hover:bg-green-600 transition duration-300 font-bold">Surveys em Espera</button>
+            <button onClick={() => handleNavigation('/respond-survey')} className="bg-yellow-500 text-white py-2 px-4 rounded-md hover:bg-yellow-600 transition duration-300 font-bold">Checar Surveys Abertas</button>
+            <button onClick={() => handleNavigation('/responded-surveys')} className="bg-purple-500 text-white py-2 px-4 rounded-md hover:bg-purple-600 transition duration-300 font-bold">Checar Resultados de Surveys</button>
+          </div>
+          {user.role === 'SurveyAdmin' && (
+            <button onClick={() => navigate('/user-management')} className="bg-red-500 text-white py-2 px-4 rounded-md hover:bg-red-600 transition duration-300 font-bold">Gerenciar Usuários</button>
+          )}
+          <div className="mt-4">
+            <LogoutButton />
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
