c1689aea0bf93a66787646f7ae9e0446be8a6de9
thallys97 <thallysbandeira97@gmail.com>
2024-04-12 14:53:59 -0500

    estilizei a survey que é selecionada

--- client/src/components/RespondedSurveys.js
+++ client/src/components/RespondedSurveys.js
@@ -87,7 +87,7 @@ const RespondedSurveys = () => {
           return (
             <div key={index} className="p-2 w-full sm:w-1/3 lg:w-1/5">
               <ErrorBoundary>
-                <div className="p-4 bg-white rounded-lg shadow-md">
+                <div className="p-2 bg-white rounded-lg shadow-md">
                   <h4 className="text-center mb-4">{result.text}</h4>
                   <Pie data={data} />
                 </div>
@@ -100,52 +100,57 @@ const RespondedSurveys = () => {
   };
 
   return (
-    <div>
+    <div className="min-h-screen bg-gray-100 flex flex-col items-center justify-center">
         {selectedSurvey ? (
-          <div>
-            <h3>{selectedSurvey.title}</h3>
-            <table>
-              <thead>
-                <tr>
-                  <th>Pergunta</th>
-                  {[...Array(maxOptionsCount())].map((_, index) => (
-                    <th key={index}>Opção {index + 1}</th>
-                  ))}
-                </tr>
-              </thead>
-              <tbody>
-                {surveyResults.map((result, index) => (
-                  <tr key={index}>
-                    {/* <td>{result.text}</td> */}
-                    <td>Pergunta {index + 1}</td>
-                    {[...Array(maxOptionsCount())].map((_, cIndex) => {
-                      // Verifica se existe a escolha para aquela coluna, caso contrário renderiza "N/A"
-                      const choiceResult = result.choices[cIndex];
-                      return (
-                        <td key={cIndex}>
-                          {choiceResult ? choiceResult.count : 'N/A'}
-                        </td>
-                      );
-                    })}
+          <div className="container mx-auto p-4 space-y-8">
+            <h2 className="text-3xl font-semibold text-center">{selectedSurvey.title}</h2>
+            <div className="overflow-x-auto">
+              <table className="min-w-full divide-y divide-gray-200">
+                <thead className="bg-gray-50">
+                  <tr>
+                    <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Pergunta</th>
+                    {[...Array(maxOptionsCount())].map((_, index) => (
+                      <th key={index} className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Opção {index + 1}</th>
+                    ))}
                   </tr>
-                ))}
-              </tbody>
-            </table>
-            {renderPieCharts()}
-            <button onClick={() => setSelectedSurvey(null)}>Voltar às Surveys</button>
-            <br />
-            <br />
-            <p>Perguntas e Opções</p>
-            <br />
-            <br />
-            {selectedSurvey.questions.map((question, index) => (
-              <div key={index}>
-                <p>{index + 1}. {question.text}</p>
-                {question.choices.map((choice, choiceIndex) => (
-                  <p key={choiceIndex}>{choice}</p>
-                ))}
-              </div>
-            ))}
+                </thead>
+                <tbody className="bg-white divide-y divide-gray-200">
+                  {surveyResults.map((result, index) => (
+                    <tr key={index}>
+                      {/* <td>{result.text}</td> */}
+                      <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">Pergunta {index + 1}</td>
+                      {[...Array(maxOptionsCount())].map((_, cIndex) => {
+                        // Verifica se existe a escolha para aquela coluna, caso contrário renderiza "N/A"
+                        const choiceResult = result.choices[cIndex];
+                        return (
+                          <td key={cIndex} className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                            {choiceResult ? choiceResult.count : 'N/A'}
+                          </td>
+                        );
+                      })}
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </div>
+            
+              {renderPieCharts()}
+            
+            <div className="text-center">
+              <button onClick={() => setSelectedSurvey(null)}  className="mt-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Voltar às Surveys</button>
+            </div>
+            
+            <h2 className="text-3xl font-semibold text-center my-8">Perguntas e Opções</h2>
+            <div className="container mx-auto p-4 space-y-8">
+              {selectedSurvey.questions.map((question, index) => (
+                <div key={index} className="my-4 p-4 bg-white rounded shadow">
+                  <p className="text-lg font-semibold">{index + 1}. {question.text}</p>
+                  {question.choices.map((choice, choiceIndex) => (
+                    <p key={choiceIndex} className="ml-4 pl-4 py-2 border-l-2 border-gray-200">{choice}</p>
+                  ))}
+                </div>
+              ))}
+             </div> 
           </div>
         ) : (
           <div className="min-h-screen bg-gray-100 flex justify-center items-center">
