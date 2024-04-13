7c487ac886d8809669300ea0399f0f3175a51cdf
thallys97 <thallysbandeira97@gmail.com>
2024-04-12 22:27:56 -0500

    alterei a disposição de botões em dispositivos mobile

--- client/src/components/SurveyCreator.js
+++ client/src/components/SurveyCreator.js
@@ -211,7 +211,7 @@ const SurveyCreator = () => {
                   Adicionar Pergunta
                 </button>
               )}
-            <div>
+            <div className="flex items-center justify-end">
             <button
                 type="submit"
                 className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline mr-2"
