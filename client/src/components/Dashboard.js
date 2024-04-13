c3732db6c4fb157d9c684b86dc92a35d3bcb7dd5
thallys97 <thallysbandeira97@gmail.com>
2024-04-12 22:27:10 -0500

    a funcionalide de criar survey e a de checar surveys em espera agora está restrita a coordenadores e administradores

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -18,8 +18,12 @@ const Dashboard = () => {
           <h1 className="text-2xl font-bold mb-4 text-gray-800">Dashboard</h1>
           {user && <p className="mb-6">Bem-vindo, {user.displayName}!</p>}
           <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-4 mb-4">
-            <button onClick={() => handleNavigation('/create-survey')} className="bg-blue-500 text-white py-2 px-4 rounded-md hover:bg-blue-600 transition duration-300 font-bold">Criar Survey</button>
-            <button onClick={() => handleNavigation('/waiting-surveys')} className="bg-green-500 text-white py-2 px-4 rounded-md hover:bg-green-600 transition duration-300 font-bold">Surveys em Espera</button>
+          {user.role !== 'SurveyRespondent' && (
+            <>   
+              <button onClick={() => handleNavigation('/create-survey')} className="bg-blue-500 text-white py-2 px-4 rounded-md hover:bg-blue-600 transition duration-300 font-bold">Criar Survey</button>   
+              <button onClick={() => handleNavigation('/waiting-surveys')} className="bg-green-500 text-white py-2 px-4 rounded-md hover:bg-green-600 transition duration-300 font-bold">Surveys em Espera</button>
+            </>  
+          ) }
             <button onClick={() => handleNavigation('/respond-survey')} className="bg-yellow-500 text-white py-2 px-4 rounded-md hover:bg-yellow-600 transition duration-300 font-bold">Checar Surveys Abertas</button>
             <button onClick={() => handleNavigation('/responded-surveys')} className="bg-purple-500 text-white py-2 px-4 rounded-md hover:bg-purple-600 transition duration-300 font-bold">Checar Resultados de Surveys</button>
           </div>
