74d2642cd6487493dccc843a9143d944f9acdd96
thallys97 <thallysbandeira97@gmail.com>
2024-04-12 22:30:12 -0500

    coloquei middlewares em algumas rotas da aplicação

--- server/routes/surveyRoutes.js
+++ server/routes/surveyRoutes.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const surveyController = require('../controllers/surveyController');
-const { ensureAuth, ensureCoordinatorOrAdmin } = require('../middlewares/authMiddleware');
+const { ensureAuth, ensureCoordinatorOrAdmin, ensureRespondent } = require('../middlewares/authMiddleware');
 
 
 // POST para criar uma nova survey - apenas usuários autenticados
 router.post('/', ensureAuth, ensureCoordinatorOrAdmin, surveyController.createSurvey);
 
-router.get('/waiting', ensureAuth, surveyController.getWaitingSurveys);
+router.get('/waiting', ensureAuth, ensureCoordinatorOrAdmin, surveyController.getWaitingSurveys);
 
 router.get('/responded', ensureAuth, surveyController.getRespondedSurveys);
 
@@ -15,13 +15,13 @@ router.get('/responded', ensureAuth, surveyController.getRespondedSurveys);
 router.get('/results/:id', ensureAuth, surveyController.getSurveyResults);
 
 // GET para listar todas as surveys
-router.get('/', surveyController.getAllSurveys);
+router.get('/', ensureAuth, surveyController.getAllSurveys);
 
 // DELETE para deletar uma survey
 router.delete('/:id', ensureAuth, ensureCoordinatorOrAdmin, surveyController.deleteSurvey);
 
 // GET para obter todas as surveys abertas
-router.get('/open', surveyController.getOpenSurveys);
+router.get('/open', ensureAuth, surveyController.getOpenSurveys);
 
 // GET para obter uma survey específica pelo ID
 router.get('/:id', ensureAuth, surveyController.getSurveyById);
@@ -33,6 +33,6 @@ router.put('/open/:id', ensureAuth, ensureCoordinatorOrAdmin, surveyController.o
 router.put('/close/:id', ensureAuth, ensureCoordinatorOrAdmin, surveyController.closeSurvey);
 
 // Endpoint para submeter resposta para uma survey
-router.post('/response', ensureAuth, surveyController.submitResponse);
+router.post('/response', ensureAuth, ensureRespondent, surveyController.submitResponse);
 
 module.exports = router;
