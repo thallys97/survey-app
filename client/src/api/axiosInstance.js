cda80d4ccb32ace661ce2f7788fd91dc108b0f0f
thallys97 <thallysbandeira97@gmail.com>
2024-04-14 13:32:29 -0500

    alterei a url base do axios

--- client/src/api/axiosInstance.js
+++ client/src/api/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:5000',
+  baseURL: 'https://survey-api-app.vercel.app',
   withCredentials: true // Necessário para enviar e receber cookies
 });
 
