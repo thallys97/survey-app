cfb2cd8e796848f8fc2f16cdd0d116c637b30681
thallys97 <thallysbandeira97@gmail.com>
2024-04-15 23:41:01 -0500

    atualizei os middlewares para lidar com autenticação JWT

--- server/middlewares/authMiddleware.js
+++ server/middlewares/authMiddleware.js
@@ -1,34 +1,78 @@
-module.exports = {
-    ensureAuth: function (req, res, next) {
-      if (req.isAuthenticated()) { // Passport adiciona o método isAuthenticated() ao req
-        return next();
-      } else {
-        res.status(401).json({ message: 'Usuário não autenticado' });
-      }
-    },
+const jwt = require('jsonwebtoken');
 
-    ensureRespondent: function (req, res, next) {
-      if (req.user.role === 'SurveyRespondent') {
-        return next();
-      } else {
-        res.status(403).json({ message: 'Acesso negado' });
-      }
-    },
 
-    ensureCoordinatorOrAdmin: function (req, res, next) {
-      if (req.user.role === 'SurveyCoordinator' || req.user.role === 'SurveyAdmin') {
-        return next();
-      } else {
-        res.status(403).json({ message: 'Acesso negado' });
-      }
-    },
+const verifyToken = (req, res) => {
+  let token = req.headers.authorization;
 
-    ensureAdmin: function (req, res, next) {
-      if (req.user.role === 'SurveyAdmin') {
-        return next();
-      } else {
-        res.status(403).json({ message: 'Acesso restrito apenas para administradores' });
-      }
-    }
-  };
+  if (token && token.startsWith('Bearer ')) {
+    // Retira 'Bearer ' do início
+    token = token.slice(7, token.length);
+  } else {
+    // Se não houver token, retorna um erro
+    return { error: 'Token não fornecido' };
+  }
+
+  try {
+    // Tenta verificar o token e decodifica
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    return { decoded };
+  } catch (error) {
+    // Se o token for inválido ou expirado, retorna um erro
+    return { error: 'Token inválido' };
+  }
+};
+
+const ensureAuth = function (req, res, next) {
+  const { decoded, error } = verifyToken(req, res);
+  if (error) {
+    return res.status(401).json({ message: error });
+  }
+
+  req.user = decoded;
+  next();
+};
+
+const ensureRespondent = function (req, res, next) {
+  const { decoded, error } = verifyToken(req, res);
+  if (error) {
+    return res.status(401).json({ message: error });
+  }
+
+  if (decoded.role === 'SurveyRespondent') {
+    req.user = decoded;
+    next();
+  } else {
+    return res.status(403).json({ message: 'Acesso negado' });
+  }
+};
+
+const ensureCoordinatorOrAdmin = function (req, res, next) {
+  const { decoded, error } = verifyToken(req, res);
+  if (error) {
+    return res.status(401).json({ message: error });
+  }
+
+  if (decoded.role === 'SurveyCoordinator' || decoded.role === 'SurveyAdmin') {
+    req.user = decoded;
+    next();
+  } else {
+    return res.status(403).json({ message: 'Acesso negado' });
+  }
+};
+
+const ensureAdmin = function (req, res, next) {
+  const { decoded, error } = verifyToken(req, res);
+  if (error) {
+    return res.status(401).json({ message: error });
+  }
+
+  if (decoded.role === 'SurveyAdmin') {
+    req.user = decoded;
+    next();
+  } else {
+    return res.status(403).json({ message: 'Acesso restrito apenas para administradores' });
+  }
+};
+
+module.exports = { ensureAuth, ensureRespondent, ensureCoordinatorOrAdmin, ensureAdmin };
 
