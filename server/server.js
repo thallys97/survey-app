e86d2b79d553cc2c2b07fee0860fe20d15f998e3
thallys97 <thallysbandeira97@gmail.com>
2024-04-15 23:42:15 -0500

    atualizei o server.js para lidar com autenticação JWT

--- server/server.js
+++ server/server.js
@@ -2,9 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const cors = require('cors');
-const session = require('express-session');
 const passport = require('passport');
-const MongoStore = require('connect-mongo');
 const authRoutes = require('./routes/authRoutes');
 const surveyRoutes = require('./routes/surveyRoutes');
 const userRoutes = require('./routes/userRoutes');
@@ -13,9 +11,7 @@ const userRoutes = require('./routes/userRoutes');
 dotenv.config();
 
 // Conectar ao MongoDB
-
 mongoose.set('strictQuery', false);
-
 const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGO_URI);
@@ -31,43 +27,20 @@ const app = express();
 
 // Configuração do CORS
 const corsOptions = {
-  origin: process.env.CLIENT_URL, // ou o URL do seu front-end em produção
-  credentials: true, // permite enviar e receber cookies
+  origin: process.env.CLIENT_URL,
+  credentials: true,
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   preflightContinue: false,
   optionsSuccessStatus: 204
 };
-
-app.use(cors(corsOptions)); // Use CORS antes das suas rotas
+app.use(cors(corsOptions));
 
 // Middleware para analisar JSON
 app.use(express.json());
 
-// Configuração do express-session
-app.use(session({
-  secret: process.env.SESSION_SECRET,
-  resave: false,
-  saveUninitialized: false,
-  store: MongoStore.create({ mongoUrl: process.env.MONGO_URI }),
-  cookie: {
-    httpOnly: true,
-    secure: process.env.NODE_ENV === 'production', // Só envia o cookie com requisições HTTPS se estiver em produção
-    sameSite: process.env.NODE_ENV === 'production' ? 'None' : 'Lax', // Usa 'None' para CORS em produção
-    // Outras configurações de cookie podem ser colocadas aqui
-  }
-}));
-
-
 // Configuração do Passport
-require('./config/passport')(passport); // Config do Passport
-
+require('./config/passport')(passport);
 app.use(passport.initialize());
-app.use(passport.session()); // Se você está usando sessões
-
-
-
-// Importar rotas
-
 
 // Usar rotas
 app.use('/auth', authRoutes);
@@ -81,7 +54,6 @@ app.get('/', (req, res) => {
 
 // Iniciar o servidor
 const PORT = process.env.PORT || 5000;
-
 connectDB().then(() => {
   app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 });
