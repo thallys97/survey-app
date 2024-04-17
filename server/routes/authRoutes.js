a5696cbeaddb9f3ecc64ff386c08ba0206ee2f20
thallys97 <thallysbandeira97@gmail.com>
2024-04-17 00:00:36 -0500

    no back end criei uma rota para validar o token, comentei a rota de logout e simplifiquei a rota de /current_user

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const passport = require('passport');
 const jwt = require('jsonwebtoken');
+const { ensureAuth } = require('../middlewares/authMiddleware');
 const router = express.Router();
 
 // Rota para autenticar com Google
@@ -36,31 +37,53 @@ router.get('/google/callback',
   }
 );
 
-router.get('/current_user', (req, res) => {
-  const { decoded, error } = verifyToken(req, res);
-  if (error) {
-      return res.status(401).json({ message: 'Unauthorized: No token provided or token is invalid' });
+// Rota para validar token JWT
+router.get('/validate_token', ensureAuth, (req, res) => {
+  // Se o token for válido, o middleware 'ensureAuth' adicionará o usuário ao 'req'
+  if (req.user) {
+    res.json({
+      success: true,
+      user: req.user
+    });
+  } else {
+    res.status(401).json({
+      success: false,
+      message: 'Token inválido ou expirado'
+    });
   }
-  // Optionally, verify the user still exists in the database here
-  res.json({
-      id: decoded.id,
-      displayName: decoded.displayName,
-      email: decoded.email,
-      role: decoded.role
-  });
 });
 
-router.get('/logout', (req, res) => {
-  req.logout((error) => {
-    if (error) { 
-      return next(error);
-    }
-    req.session.destroy(() => {
-      res.clearCookie('connect.sid'); // The name of the cookie may be different based on your configuration
-      res.redirect(process.env.CLIENT_URL);
-    });
-  });
+
+router.get('/current_user', ensureAuth, (req, res) => {
+  res.json(req.user);
 });
+
+
+// router.get('/current_user', (req, res) => {
+//   const { decoded, error } = verifyToken(req, res);
+//   if (error) {
+//       return res.status(401).json({ message: 'Unauthorized: No token provided or token is invalid' });
+//   }
+//   // Optionally, verify the user still exists in the database here
+//   res.json({
+//       id: decoded.id,
+//       displayName: decoded.displayName,
+//       email: decoded.email,
+//       role: decoded.role
+//   });
+// });
+
+// router.get('/logout', (req, res) => {
+//   req.logout((error) => {
+//     if (error) { 
+//       return next(error);
+//     }
+//     req.session.destroy(() => {
+//       res.clearCookie('connect.sid'); // The name of the cookie may be different based on your configuration
+//       res.redirect(process.env.CLIENT_URL);
+//     });
+//   });
+// });
 
 // Exportar as rotas
 module.exports = router;
