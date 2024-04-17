b9ae0a941ff1ec4b1cc1826ae322eb817d2d6791
thallys97 <thallysbandeira97@gmail.com>
2024-04-16 23:58:03 -0500

    atualizei o AuthContext para lidar melhor com a autenticação por token

--- client/src/contexts/AuthContext.js
+++ client/src/contexts/AuthContext.js
@@ -1,33 +1,57 @@
 import React, { createContext, useState, useEffect } from 'react';
-import authService from '../services/authService'; // Importe seu authService
+import authService from '../services/authService';
+import { useLocation } from 'react-router-dom';
 
 export const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null);
   const [loading, setLoading] = useState(true);
+  const location = useLocation();
 
   useEffect(() => {
-    const fetchCurrentUser = async () => {
-      try {
-        const fetchedUser = await authService.fetchCurrentUser();
-        if (fetchedUser) {
-          setUser(fetchedUser);
+    const extractToken = () => {
+      const query = new URLSearchParams(location.search);
+      return query.get('token');
+    };
+  
+    const initializeAuth = async () => {
+      const token = extractToken();
+      if (token) {
+        localStorage.setItem('token', token);
+        const validatedUser = await authService.validateToken(token);
+        if (validatedUser) {
+          setUser(validatedUser);
+        } else {
+          console.error("Token validation failed");
         }
-      } catch (error) {
-        console.error('Erro ao buscar o usuário atual', error);
+      } else {
+        // Só tenta buscar o usuário se não estiver lidando com um novo token
+        if (localStorage.getItem('token')) {
+          try {
+            const fetchedUser = await authService.fetchCurrentUser();
+            setUser(fetchedUser);
+          } catch (error) {
+            console.error('Error fetching current user', error);
+          }
+        }
       }
       setLoading(false);
     };
   
-    fetchCurrentUser();
-  }, []);
+    initializeAuth();
+  }, [location]);
 
-  // Use as funções do authService diretamente
-  const login = authService.login;
+  const login = () => {
+    authService.login();
+  };
+
   const logout = async () => {
-    await authService.logout();
-    setUser(null); // Atualize o estado do usuário no contexto
+    //await authService.logout();
+    localStorage.removeItem('token');
+    setUser(null);
+    window.location.href = '/';
+    window.location.reload();
   };
 
   return (
