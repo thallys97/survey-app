2ea2af413d647eda24dd9633893968c107eafc0f
thallys97 <thallysbandeira97@gmail.com>
2024-04-17 20:27:33 -0500

    atualizei o dashboard

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -5,6 +5,7 @@ import LogoutButton from './LogoutButton';
 import { useNavigate } from 'react-router-dom';
 
 const Dashboard = () => {
+  window.location.reload();
   const { user } = useContext(AuthContext);
   const navigate = useNavigate();
 
